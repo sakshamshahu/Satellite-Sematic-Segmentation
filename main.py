7db0c35cd37887e1c7bbd07a5cc5244d7c573636
sakshamshahu <sakshamshahu.work@gmail.com>
2024-04-14 17:06:18 +0530

    feat: read and convert imgs into req format
    Signed-off-by: sakshamshahu <sakshamshahu.work@gmail.com>

--- main.py
+++ main.py
@@ -7,3 +7,41 @@ Created on Fri Apr 12 13:55:10 2024
 """
 
 import tensorflow as tf
+import os
+import cv2
+import numpy as np 
+from matplotlib import pyplot as plt
+from PIL import Image
+from patchify import patchify
+
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+scaler = MinMaxScaler()
+
+
+patch_size = 256
+
+dir_root = '/Users/saksham/Projects/Unet/data'
+
+img_dataset = []
+
+for path, subdirs, files in os.walk(dir_root):
+    # print(path)
+    dirname = path.split(os.path.sep)[-1]
+    print(dirname)
+    if dirname == 'Image':
+        images = os.listdir(path)
+        for i, image_name in enumerate(images):
+            if image_name.endswith('JPG'):
+                image = cv2.imread(path + '/' + image_name, 1)
+                
+                print('Creating smaller patches', path+'/'+ image_name)
+                patches_img = patchify(image, (patch_size, patch_size, 3), step=patch_size)
+                for i in range(patches_img.shape[0]):
+                    for j in range(patches_img.shape[1]):
+                        
+                        single_patch_img = patches_img[i,j,:,:]
+                        single_patch_img = scaler.fit_transform(single_patch_img.reshape(-1, single_patch_img.shape[-1])).reshape(single_patch_img.shape)
+                    
+                        single_patch_img = single_patch_img[0] #Drop the extra unecessary dimension that patchify adds.                               
+                        img_dataset.append(single_patch_img)
+                
